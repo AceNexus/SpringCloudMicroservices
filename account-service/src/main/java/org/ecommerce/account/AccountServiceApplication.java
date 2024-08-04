22778ac2e36632ef0a0246c9b3941af4c8140f0a
Ace <ooxxqaws@gmail.com>
2024-08-04 22:06:46 +0800

    <account-1.0.3> [feat] 將帳號系統註冊到 Eureka

--- account-service/src/main/java/org/ecommerce/account/AccountServiceApplication.java
+++ account-service/src/main/java/org/ecommerce/account/AccountServiceApplication.java
@@ -2,8 +2,10 @@ package org.ecommerce.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient // 此服務啟動後自動註冊至 eureka
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
