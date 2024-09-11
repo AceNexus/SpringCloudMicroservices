43c63faf937134f300184633484aea5b36561fca
Ace <ooxxqaws@gmail.com>
2024-09-12 00:50:33 +0800

    <common-1.0.1> [modify] 重構 ApiResponse，移除靜態方法，改用建構子進行初始化，並自定義 ApiStatusCode

--- common/src/main/java/org/ecommerce/common/utils/ApiResponse.java
+++ common/src/main/java/org/ecommerce/common/utils/ApiResponse.java
@@ -1,16 +1,12 @@
 package org.ecommerce.common.utils;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 import java.time.Instant;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
-@Accessors(chain = true)
 public class ApiResponse<T> {
 
     private Instant timestamp;
@@ -18,8 +14,11 @@ public class ApiResponse<T> {
     private String message;
     private T data;
 
-    public static <T> ApiResponse<T> create(int status, String message, T data) {
-        return new ApiResponse<>(Instant.now(), status, message, data);
+    public ApiResponse(int status, String message, T data) {
+        this.timestamp = Instant.now();
+        this.status = status;
+        this.message = message;
+        this.data = data;
     }
 
 }
---
+++ common/src/main/java/org/ecommerce/common/utils/ApiStatusCode.java
@@ -0,0 +1,7 @@
+package org.ecommerce.common.utils;
+
+public class ApiStatusCode {
+
+    public static final int SUCCESS = 0;
+
+}
